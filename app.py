8b06f00152c586b39bba4c71ce38652023436708
Taron Osifo <taron.osifo1@gmail.com>
2026-04-14 02:16:31 -0500

    Comparison UI and for both models

--- app.py
+++ app.py
@@ -14,21 +14,41 @@ class LogisticRegression(nn.Module):
         return self.linear(x)
 
 
-@st.cache_resource
-def load_model():
-    model = LogisticRegression(input_dim=12, num_classes=3)
-    model.load_state_dict(torch.load("model.pt", map_location="cpu"))
-    model.eval()
-    return model
+class NeuralNetModel(nn.Module):
+    def __init__(self, input_dim=12, num_classes=3):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, num_classes)
+        )
 
-model = load_model()
+    def forward(self, x):
+        return self.net(x)
+
+
+
+@st.cache_resource
+def load_models():
+    model = LogisticRegression()
+    model.load_state_dict(torch.load("logreg_model.pt", map_location="cpu"))
+    model.eval()
+
+    nn_model = NeuralNetModel()
+    nn_model.load_state_dict(torch.load("nn_model.pt", map_location="cpu"))
+    nn_model.eval()
+
+    return lr_model, nn_model
+
+lr_model, nn_model = load_models()
 
 
 def tokenize(text):
     text = text.lower()
     text = re.sub(r"[^\w\s]", "", text)
     return text.split()
-
 
 def extract_features(text):
     words = tokenize(text)
@@ -37,34 +57,28 @@ def extract_features(text):
     longest_word = max([len(w) for w in words]) if words else 1
     long_words = len([w for w in words if len(w) >= 5])
 
+    return np.array([
+        0,0,0,0,0,0,0,0,0,
+        np.log(word_count + 1),
+        np.log(longest_word + 1),
+        np.log(long_words + 1)
+    ], dtype=np.float32)
 
-    f1 = 0
-    f2 = 0
-    f3 = 0
-    f4 = 0
-    f5 = 0
-    f6 = 0
-    f7 = 0
-    f8 = 0
-    f9 = 0
-    f10 = np.log(word_count + 1)
-    f11 = np.log(longest_word + 1)
-    f12 = np.log(long_words + 1)
-
-    return np.array([f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12], dtype=np.float32)
 
 st.title("Reddit Sentiment Analyzer")
-st.write("Enter text to classify sentiment (Negative / Neutral / Positive)")
 
-user_input = st.text_area("Input Text")
+model_choice = st.radio("Choose Model", ["Logistic Regression", "Neural Network"])
+
+text = st.text_area("Enter text")
 
 labels = ["Negative", "Neutral", "Positive"]
 
 
 if st.button("Predict"):
 
-    features = extract_features(user_input)
-    features = torch.tensor(features).unsqueeze(0)
+    features = torch.tensor(extract_features(text)).unsqueeze(0)
+
+    model = lr_model if model_choice == "Logistic Regression" else nn_model
 
     with torch.no_grad():
         logits = model(features)
@@ -83,4 +97,4 @@ if st.button("Predict"):
 
 
 st.markdown("---")
-st.caption("Built using PyTorch + Streamlit")
+st.caption("Built with PyTorch + Streamlit")
